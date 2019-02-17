07190e79f5cd85d5a8c76b6c69dda4460e05f966
pinkfish <pinkfishfrog@gmail.com>
2019-02-16 22:44:33 -0800

    Update pubspec to new version of flutter.

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_native_timezone/main.dart';
+import 'package:flutter_native_timezone_dialog_example/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {

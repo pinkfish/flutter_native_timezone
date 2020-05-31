922cf5b78b86de57419b663328dd15501f2f7b2c
pinkfish <pinkfish@users.noreply.github.com>
2020-05-31 13:23:09 -0700

    Add an ensureInitialized call

--- test/flutter_native_timezone_test.dart
+++ test/flutter_native_timezone_test.dart
@@ -3,6 +3,8 @@ import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+    
   const MethodChannel channel = MethodChannel('flutter_native_timezone');
 
   setUp(() {

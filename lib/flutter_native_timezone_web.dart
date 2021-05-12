6abc64a2d8731b0ec9c3438919c174aeee0df840
David Bennett <pinkfishfrog@gmail.com>
2021-05-12 12:40:07 +0000

    Add in web support to the plugin.

---
+++ lib/flutter_native_timezone_web.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'dart:html' as html;
+
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:js/js.dart';
+
+///
+/// The plugin class for the web, acts as the plugin inside bits
+/// and connects to the js world.
+///
+class FlutterNativeTimezonePlugin {
+  static void registerWith(Registrar registrar) {
+    final MethodChannel channel = MethodChannel(
+        'flutter_native_timezone',
+        const StandardMethodCodec(),
+        registrar.messenger);
+    final FlutterNativeTimezonePlugin instance = FlutterNativeTimezonePlugin();
+    channel.setMethodCallHandler(instance.handleMethodCall);
+  }
+
+  Future<dynamic> handleMethodCall(MethodCall call) async {
+    switch (call.method) {
+      case 'getLocalTimezone':
+        return _getLocalTimeZone();
+      case 'getAvailableTimezones':
+        return [ _getLocalTimeZone() ];
+      default:
+        throw PlatformException(
+            code: 'Unimplemented',
+            details: "The flutter_native_timezone plugin for web doesn't implement "
+                "the method '${call.method}'");
+    }
+  }
+
+  /// Platform-specific implementation of determining the user's
+  /// local time zone when running on the web.
+  ///
+  String _getLocalTimeZone() {
+    return jsDateTimeFormat()
+        .resolvedOptions()
+        .timeZone;
+  }
+}
+
+@JS('Intl.DateTimeFormat')
+external _JSDateTimeFormat jsDateTimeFormat();
+
+@JS()
+abstract class _JSDateTimeFormat {
+  @JS()
+  external _JSResolvedOptions resolvedOptions();
+}
+
+@JS()
+abstract class _JSResolvedOptions {
+  @JS()
+  external String get timeZone;
+}
+
